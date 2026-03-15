4c67d5def0dd8784f8b745f58859f9db49e091b1
ban.der4 <ctepanskinder@yandex.ru>
2026-03-15 21:52:32 +0300

    add func GetUserBooks

--- internal/services/service.go
+++ internal/services/service.go
@@ -34,23 +34,36 @@ func (s *Service) CreateUser(ctx context.Context, telegramID int64, username str
 
 func (s *Service) GetUserByTelegramID(ctx context.Context, telegramID int64) (*model.User, error) {
 	// TODO : request validation
+
 	const op = "Service.GetUserByTelegramID"
 	l := s.log.With(slog.String("op", op), slog.Int64("telegramID", telegramID))
 	l.Info("attempting to get user info")
-	data, err := s.storage.GetUserByTelegramID(ctx, telegramID)
+
+	userData, err := s.storage.GetUserByTelegramID(ctx, telegramID)
 	if err != nil {
 		l.Error("Failed to get user info", slog.Any("err", err))
 		return nil, fmt.Errorf("%s:%w", op, err)
 	}
-	return data, nil
+	// think about where and how to process empty data from the database
+	return userData, nil
 }
 
 func (s *Service) CreateBook(ctx context.Context, book *model.Book) error {
 	panic("implement me")
 }
 
-func (s *Service) GetUserBook(ctx context.Context, userID int64) ([]*model.Book, error) {
-	panic("implement me")
+func (s *Service) GetUserBooks(ctx context.Context, userID int64) ([]*model.Book, error) {
+	const op = "Service.GetUserBook"
+	l := s.log.With(slog.String("op", op), slog.Int64("userID", userID))
+	l.Info("attempting to get user books")
+
+	booksData, err := s.storage.GetUserBooks(ctx, userID)
+	if err != nil {
+		l.Error("Failed to get user books", slog.Any("err", err))
+		return nil, fmt.Errorf("%s:%w", op, err)
+	}
+	// think about where and how to process empty data from the database
+	return booksData, nil
 }
 
 func (s *Service) UpdateBookProgress(ctx context.Context, bookID int64, pagesRead int64) error {
