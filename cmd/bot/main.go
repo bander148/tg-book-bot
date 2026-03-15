e2a44da8d44040df3f3bfd706715c14d63a8359e
ban.der4 <ctepanskinder@yandex.ru>
2026-03-15 21:25:09 +0300

    add health check,setup logger

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"log/slog"
+	"os"
+	"tgBookBot/internal/config"
+
+	"gopkg.in/telebot.v4"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	logger := setapLogger(cfg.Env)
+	b, err := telebot.NewBot(telebot.Settings{Token: cfg.TGBotConfig.APIToken, Poller: &telebot.LongPoller{Timeout: cfg.TGBotConfig.PollTime}})
+	if err != nil {
+		logger.Error("can't start bot", slog.Any("err", err))
+		panic("Failed to start bot")
+	}
+	logger.Info("Bot started success")
+	b.Handle("/health", func(c telebot.Context) error {
+		return c.Send("OK")
+	})
+	b.Start()
+}
+func setapLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
