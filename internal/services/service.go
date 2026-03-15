ed33db01d2fc058bae16b4e572f23dd6864f4806
ban.der4 <ctepanskinder@yandex.ru>
2026-03-15 21:44:22 +0300

    add func GetUserByTelegramID

--- internal/services/service.go
+++ internal/services/service.go
@@ -18,21 +18,31 @@ func New(log *slog.Logger, storage storage.Storage) *Service {
 	return &Service{log: log, storage: storage}
 }
 func (s *Service) CreateUser(ctx context.Context, telegramID int64, username string) error {
+	// TODO : request validation
 	const op = "Service.CreateUser"
-	log := s.log.With(slog.String("op", op), slog.Int64("telegramID", telegramID))
-	log.Info("attempting to create new user")
+	l := s.log.With(slog.String("op", op), slog.Int64("telegramID", telegramID))
+	l.Info("attempting to create new user")
 
 	date := time.Now()
 	if err := s.storage.CreateUser(ctx, telegramID, username, date); err != nil {
-		log.Error("failed to create user", slog.Any("error", err))
+		l.Error("failed to create user", slog.Any("error", err))
 		return fmt.Errorf("%s:%w", op, err)
 	}
-	log.Info("user created")
+	l.Info("user created")
 	return nil
 }
 
 func (s *Service) GetUserByTelegramID(ctx context.Context, telegramID int64) (*model.User, error) {
-	panic("implement me")
+	// TODO : request validation
+	const op = "Service.GetUserByTelegramID"
+	l := s.log.With(slog.String("op", op), slog.Int64("telegramID", telegramID))
+	l.Info("attempting to get user info")
+	data, err := s.storage.GetUserByTelegramID(ctx, telegramID)
+	if err != nil {
+		l.Error("Failed to get user info", slog.Any("err", err))
+		return nil, fmt.Errorf("%s:%w", op, err)
+	}
+	return data, nil
 }
 
 func (s *Service) CreateBook(ctx context.Context, book *model.Book) error {
