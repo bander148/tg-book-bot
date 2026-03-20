25fc11a7bce2df215c0622eb403e0b59ae74a05a
ban.der4 <ctepanskinder@yandex.ru>
2026-03-20 20:18:02 +0300

    add repository

---
+++ internal/storage/postgresql/repository.go
@@ -0,0 +1,111 @@
+package postgresql
+
+import (
+	"context"
+	"log/slog"
+	model "tgBookBot/internal/domain/models"
+	"tgBookBot/internal/storage"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type pgRepository struct {
+	client Client
+	log    *slog.Logger
+}
+
+func NewRepository(client Client, log *slog.Logger) storage.Storage {
+	return &pgRepository{client: client, log: log}
+}
+
+// TODO : use dto for operations - userDTO
+func (p *pgRepository) CreateUser(ctx context.Context, telegramID int64, username string) (int, error) {
+	const op = "pdRepository.CreateUser"
+	var id int
+	l := p.log.With(slog.String("op", op), slog.Int64("tg_id", telegramID))
+	q := `INSERT INTO users (telegram_id,username) VALUES ($1,$2) RETURNING id`
+	if err := p.client.QueryRow(ctx, q, telegramID, username).Scan(&id); err != nil {
+		if pgErr, ok := err.(*pgconn.PgError); ok {
+			l.Error("can't save user", slog.Any("err", pgErr.Message), slog.Any("deatil", pgErr.Detail), slog.Any("where", pgErr.Where), slog.Any("code", pgErr.Code), slog.Any("sqlstate", pgErr.SQLState()))
+			return 0, err
+		}
+
+		return 0, err
+	}
+	l.Info("user created", slog.Int("id", id))
+	return id, nil
+}
+func (p *pgRepository) GetUserByTelegramID(ctx context.Context, telegramID int64) (*model.User, error) {
+	const op = "pdRepository.GetUserByTelegramID"
+	l := p.log.With(slog.String("op", op), slog.Int64("telegramID", telegramID))
+	q := `SELECT ...` // TODO : make sql request
+	var user model.User
+	if err := p.client.QueryRow(ctx, q, telegramID).Scan(&user); err != nil {
+		if pgErr, ok := err.(*pgconn.PgError); ok {
+			l.Error("can't find user", slog.Any("err", pgErr.Message), slog.Any("deatil", pgErr.Detail), slog.Any("where", pgErr.Where), slog.Any("code", pgErr.Code), slog.Any("sqlstate", pgErr.SQLState()))
+			return nil, err
+		}
+		return nil, err
+	}
+	l.Info("user finded", slog.Any("user_name", user.Username))
+	return &user, nil
+
+}
+func (p *pgRepository) CreateBook(ctx context.Context, book *model.Book) error {
+	panic("implement me ")
+}
+func (p *pgRepository) GetUserBooks(ctx context.Context, userID int64) ([]model.Book, error) {
+	const op = "pdRepository.GetUserBooks"
+	l := p.log.With(slog.String("op", op), slog.Int64("tg_id", userID))
+
+	q := `SELECT ...` // TODO : make sql request , указывать поля а не *
+	rows, err := p.client.Query(ctx, q, userID)
+	if err != nil {
+		l.Error("can't scan row", slog.Any("err", err))
+		return []model.Book{}, nil
+	}
+
+	books := make([]model.Book, 0)
+
+	for rows.Next() {
+		var book model.Book
+
+		err := rows.Scan(&book.ID, &book.UserID, &book.Pages, &book.Description, &book.Author, &book.Title, &book.StartDate, &book.EndDate, &book.PagesRead, &book.CreatedAt, &book.UpdatedAt)
+		if err != nil {
+			l.Error("can't scan row", slog.Any("err", err))
+			return []model.Book{}, nil
+		}
+		books = append(books, book)
+	}
+
+	if err := rows.Err(); err != nil {
+		l.Error("rows error", slog.Any("err", err))
+		return []model.Book{}, nil
+	}
+	return books, nil
+
+}
+func (p *pgRepository) UpdateBookProgress(ctx context.Context, bookID int64, pagesRead int64) error {
+	panic("implement me ")
+}
+func (p *pgRepository) MarkBookFinished(ctx context.Context, bookID int64, date time.Time) error {
+	panic("implement me ")
+}
+func (p *pgRepository) DeleteBook(ctx context.Context, bookID int64) error {
+	panic("implement me ")
+}
+func (p *pgRepository) GetBookByID(ctx context.Context, bookID int64) (*model.Book, error) {
+	panic("implement me ")
+}
+func (p *pgRepository) AddReadingSession(ctx context.Context, session *model.ReadingSession) error {
+	panic("implement me ")
+}
+func (p *pgRepository) GetReadingSessionsForPeriod(
+	ctx context.Context,
+	userID int64,
+	from time.Time,
+	to time.Time,
+) ([]*model.ReadingSession, error) {
+	panic("implement me ")
+}
