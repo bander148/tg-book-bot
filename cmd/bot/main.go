3b97d9b8383e4a9caa5fd87bcfaa6820cbb875bb
ban.der4 <ctepanskinder@yandex.ru>
2026-03-21 14:57:43 +0300

    add greceful shutdown , reorganize code

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
+	"tgBookBot/internal/bot"
 	"tgBookBot/internal/config"
+	service "tgBookBot/internal/services"
 	"tgBookBot/internal/storage/postgresql"
-
-	"gopkg.in/telebot.v4"
 )
 
 const (
@@ -18,22 +20,30 @@ const (
 
 func main() {
 	cfg := config.MustLoad()
-	var ctx context.Context
+	ctx := context.Background()
 	logger := setapLogger(cfg.Env)
-	b, err := telebot.NewBot(telebot.Settings{Token: cfg.TGBotConfig.APIToken, Poller: &telebot.LongPoller{Timeout: cfg.TGBotConfig.PollTime}})
+	client, err := postgresql.NewClient(ctx, &cfg.DBConfig, logger)
 	if err != nil {
-		logger.Error("can't start bot", slog.Any("err", err))
-		panic("Failed to start bot")
+		logger.Error("Failed to create database client", slog.Any("error", err))
+		return
 	}
-	_, err = postgresql.NewClient(ctx, &cfg.DBConfig, logger)
-	if err != nil {
-		panic(err)
-	}
+	logger.Info("Database client created successfully")
+	repo := postgresql.NewRepository(client, logger)
+	logger.Info("Repository created successfully")
+	service := service.New(logger, repo)
+	logger.Info("Service created successfully")
+	b := bot.New(&cfg.TGBotConfig, service, logger)
+	logger.Info("Bot created successfully")
+	//b.engine.Handle("/health", func(c telebot.Context) error {
+	//return c.Send("OK")
+	//})
+	go b.Start()
 	logger.Info("Bot started success")
-	b.Handle("/health", func(c telebot.Context) error {
-		return c.Send("OK")
-	})
-	b.Start()
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, syscall.SIGTERM, syscall.SIGINT)
+	stopSignal := <-osSignals
+	b.Stop()
+	logger.Info("Bot graceful stopped", slog.String("signal", stopSignal.String()))
 }
 func setapLogger(env string) *slog.Logger {
 	var log *slog.Logger
